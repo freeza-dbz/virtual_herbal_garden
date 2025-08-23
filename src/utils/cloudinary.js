a715841d67417f1819c53d8a61174e3a6b9470cf
Anmol Singh <anmol2006.chauhan@gmail.com>
2025-08-23 16:58:51 +0530

    Cloudinary Connection

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from 'cloudinary';            
+import fs from 'fs';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_sceret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadonCloudinary = async (filepath) =>{
+    try{
+        if(!filepath){
+            return null;
+        }else{
+            const response = await cloudinary.uploader.upload(filepath,{
+                resource_type: "auto"
+            });
+            fs.unlinkSync(filepath);// deleting localfile path as no more needed
+            // console.log("file uploaded on cloudinary", response);
+            return response;
+        }
+    }
+    catch(error){
+        console.error("Error uploading file to Cloudinary:", error);
+        fs.unlinkSync(filepath);
+        return null;
+    }
+}
+
+export { uploadonCloudinary }
