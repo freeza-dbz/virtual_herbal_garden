9321bb1f5c74227b9dc0a21a039a92babecf68c2
Anmol Singh <anmol2006.chauhan@gmail.com>
2025-08-26 20:11:32 +0530

    User Routes: Change Password

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -224,7 +224,32 @@ const logoutUser = asyncHandler(async (req, res) => {
 })
 
 //Change Password
+const changePassword = asyncHandler(async (req, res) => {
 
+    const { oldPassword, newPassword } = req.body
+
+    const user = User.findById(user._id)
+
+    if (!user) {
+        throw new ApiError(404, "User not found")
+    }
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Incorrect old password ")
+    }
+
+    user.password = newPassword
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            {},
+            "Password updated successfully"
+        ))
+
+})
 
 
 export {
