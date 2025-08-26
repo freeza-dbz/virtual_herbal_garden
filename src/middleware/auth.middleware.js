294d4693221de7221e9ec5a44b40ae3011957b11
Anmol Singh <anmol2006.chauhan@gmail.com>
2025-08-26 15:14:25 +0530

    Authorization middleware

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,44 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiErrors.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.models.js";
+
+
+
+const verifyJWT = asyncHandler(async (req, _, next) => {
+
+    try {
+        let token = req.cookies?.accessToken;
+
+        if (!token) {
+            const authHeader = req.header("Authorizaton")
+            if (authHeader && authHeader.startsWith("Bearer ")) {
+                token = authHeader.replace("Bearer ", "").trim()
+            }
+        }
+
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request, no token provided")
+        }
+
+        if (typeof token !== "string") {
+            throw new ApiError(401, "Unauthorized request, invalid form of token provided")
+        }
+
+        if (!token.match(/^[A-Za-z0-9-._~+\/]+=*$/)) {
+            throw new ApiError(401, "Unauthorized request, invalid form of token provided")
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+        const user = await User.findById(decodedToken._id).select(
+            "-password -refreshToken"
+        )
+
+        req.user = user;
+        next()
+    } catch (error) {
+        throw new ApiError(401, error.message || "Unauthorized request, invalid token provided")
+    }
+
+})
